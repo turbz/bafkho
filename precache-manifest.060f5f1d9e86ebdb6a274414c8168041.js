29ea33d9d4ebacd34fb4b67967a9d3ca31066b50
thabisoddx <thabisomoha@gmail.com>
2020-07-20 17:01:37 +0200

    Updates

---
+++ precache-manifest.060f5f1d9e86ebdb6a274414c8168041.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea6e69f4450f0ebf5feabc1ac055ab4c",
+    "url": "/bafkho/index.html"
+  },
+  {
+    "revision": "b3117b41d27681038be6",
+    "url": "/bafkho/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "8f6d9c312dfc36f12a00",
+    "url": "/bafkho/static/css/main.b2e951ea.chunk.css"
+  },
+  {
+    "revision": "b3117b41d27681038be6",
+    "url": "/bafkho/static/js/2.e5106017.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bafkho/static/js/2.e5106017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f6d9c312dfc36f12a00",
+    "url": "/bafkho/static/js/main.e146babb.chunk.js"
+  },
+  {
+    "revision": "237ee3098c57c0a68ee3",
+    "url": "/bafkho/static/js/runtime-main.e35e67c4.js"
+  },
+  {
+    "revision": "44e993c76c098db1b5356b8660da0d5e",
+    "url": "/bafkho/static/media/Polish15.44e993c7.jpg"
+  },
+  {
+    "revision": "e754c4e171ec2fd5b8ecf7becdd2fc90",
+    "url": "/bafkho/static/media/WK828R.e754c4e1.jpg"
+  },
+  {
+    "revision": "c9f37c1dfd10c9053dc2cc7c803640e5",
+    "url": "/bafkho/static/media/bafkho-logo.c9f37c1d.png"
+  },
+  {
+    "revision": "74d374c4d96b8223b351b5b29c4bc125",
+    "url": "/bafkho/static/media/bag.74d374c4.svg"
+  },
+  {
+    "revision": "f708e7c996ceaee050c54dcddecfec4a",
+    "url": "/bafkho/static/media/bill.f708e7c9.svg"
+  },
+  {
+    "revision": "6616af6c9c6ba769d9b640e90491ba59",
+    "url": "/bafkho/static/media/christian-mackie-SxBca4GcC9k-unsplash.6616af6c.jpg"
+  },
+  {
+    "revision": "ee76743ff81bb72d1282fa4b0954f5f3",
+    "url": "/bafkho/static/media/close-menu-icon.ee76743f.svg"
+  },
+  {
+    "revision": "db0bce9ee54e3b6712005b7b697618d9",
+    "url": "/bafkho/static/media/delivery.db0bce9e.svg"
+  },
+  {
+    "revision": "44cbc3cc3a751c6c214234492933f193",
+    "url": "/bafkho/static/media/facebook.44cbc3cc.svg"
+  },
+  {
+    "revision": "34ecc3a042d09301fb870dec86c01831",
+    "url": "/bafkho/static/media/headset.34ecc3a0.svg"
+  },
+  {
+    "revision": "b9989740d0441a5b2d9e78aa56b03625",
+    "url": "/bafkho/static/media/heart-white.b9989740.svg"
+  },
+  {
+    "revision": "874c6bae00ca5a5b3e70d966632d43d6",
+    "url": "/bafkho/static/media/heart.874c6bae.svg"
+  },
+  {
+    "revision": "972753049f5ee27e32661857a0bff693",
+    "url": "/bafkho/static/media/instagram.97275304.svg"
+  },
+  {
+    "revision": "c3d47990a77dac32919ba7713ad36d29",
+    "url": "/bafkho/static/media/menu-icon.c3d47990.svg"
+  },
+  {
+    "revision": "fddeacfecd74958525086f89f6e2a313",
+    "url": "/bafkho/static/media/menu.fddeacfe.svg"
+  },
+  {
+    "revision": "88eb9fe543091e241d5dc71e0a117b2b",
+    "url": "/bafkho/static/media/mobicred.88eb9fe5.png"
+  },
+  {
+    "revision": "2026b17a020ad9a9b06934e267c6cae6",
+    "url": "/bafkho/static/media/right-arrow.2026b17a.svg"
+  },
+  {
+    "revision": "2e78125c30f61e4de24c17b07a90f06a",
+    "url": "/bafkho/static/media/search.2e78125c.svg"
+  },
+  {
+    "revision": "348670d304b607700435712d36290286",
+    "url": "/bafkho/static/media/tag.348670d3.svg"
+  },
+  {
+    "revision": "ae5cc0ef590d84ab0c91f1d7ff8551b2",
+    "url": "/bafkho/static/media/twitter.ae5cc0ef.svg"
+  },
+  {
+    "revision": "ed0e65fc70f7364947ed37ebbde1d288",
+    "url": "/bafkho/static/media/wallet.ed0e65fc.svg"
+  },
+  {
+    "revision": "db33aed28075221a1593cf0490c0b21d",
+    "url": "/bafkho/static/media/whatsapp.db33aed2.svg"
+  },
+  {
+    "revision": "2f8dc0085d37cbfee9169d022c915a92",
+    "url": "/bafkho/static/media/zapper.2f8dc008.png"
+  }
+]);
